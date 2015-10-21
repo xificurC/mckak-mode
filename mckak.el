92c7796da2bc27e84f647d7d25ba49d3f9d78c8d
xificurC <xificurC@gmail.com>
2015-10-21 12:00:32 +0200

    Added motion commands that keep selection (SHIFT)
    - removed lexical binding for now

--- mckak.el
+++ mckak.el
@@ -1,63 +1,131 @@
-;; -*- lexical-binding: t; -*-
-
 (require 'multiple-cursors)
 (require 'cl)
+
+(defun mk--select-to-next-word-start (count &optional keep)
+  "Selects up to next word start."
+  (dotimes (_ count)
+    (unless (and keep mark-active)
+      (set-mark (point)))
+    (skip-syntax-forward "w")
+    (skip-syntax-forward "^w")))
 
 (defun mk/select-to-next-word-start (&optional count)
   "Selects up to next word start."
   (interactive "p")
   (setq count (or count 1))
+  (mk--select-to-next-word-start count))
+
+(defun mk/select-to-next-word-start-keep (&optional count)
+  "Selects up to next word start, adding to the selection."
+  (interactive "p")
+  (setq count (or count 1))
+  (mk--select-to-next-word-start count t))
+
+(defun mk--select-to-next-word-end (count &optional keep)
+  "Selects up to next word end."
   (dotimes (_ count)
-    (set-mark (point))
-    (skip-syntax-forward "w")
-    (skip-syntax-forward "^w")))
+    (unless (and keep mark-active)
+      (set-mark (point)))
+    (skip-syntax-forward "^w")
+    (skip-syntax-forward "w")))
 
 (defun mk/select-to-next-word-end (&optional count)
   "Selects up to next word end."
   (interactive "p")
   (setq count (or count 1))
+  (mk--select-to-next-word-end count))
+
+(defun mk/select-to-next-word-end-keep (&optional count)
+  "Selects up to next word end."
+  (interactive "p")
+  (setq count (or count 1))
+  (mk--select-to-next-word-end count t))
+
+(defun mk--select-to-previous-word-start (count &optional keep)
+  "Selects up to previous word start."
   (dotimes (_ count)
-    (set-mark (point))
-    (skip-syntax-forward "^w")
-    (skip-syntax-forward "w")))
+    (unless (and keep mark-active)
+      (set-mark (point)))
+    (skip-syntax-backward "^w")
+    (skip-syntax-backward "w")))
 
 (defun mk/select-to-previous-word-start (&optional count)
   "Selects up to previous word start."
   (interactive "p")
   (setq count (or count 1))
+  (mk--select-to-previous-word-start count))
+
+(defun mk/select-to-previous-word-start-keep (&optional count)
+  "Selects up to previous word start."
+  (interactive "p")
+  (setq count (or count 1))
+  (mk--select-to-previous-word-start count t))
+
+(defun mk--select-to-previous-word-end (count &optional keep)
+  "Selects up to previous word end."
   (dotimes (_ count)
-    (set-mark (point))
-    (skip-syntax-backward "^w")
-    (skip-syntax-backward "w")))
+    (unless (and keep mark-active)
+      (set-mark (point)))
+    (skip-syntax-backward "w")
+    (skip-syntax-backward "^w")))
 
 (defun mk/select-to-previous-word-end (&optional count)
   "Selects up to previous word end."
   (interactive "p")
   (setq count (or count 1))
+  (mk--select-to-previous-word-end count))
+
+(defun mk/select-to-previous-word-end-keep (&optional count)
+  "Selects up to previous word end."
+  (interactive "p")
+  (setq count (or count 1))
+  (mk--select-to-previous-word-end count t))
+
+(defun mk--forward-char (count &optional keep)
+  "Moves forward COUNT or 1 chars but stays on current line"
   (dotimes (_ count)
-    (set-mark (point))
-    (skip-syntax-backward "w")
-    (skip-syntax-backward "^w")))
+    (if keep
+        (unless mark-active
+          (set-mark (point)))
+      (deactivate-mark))
+    (save-restriction
+      (narrow-to-region (point) (line-end-position))
+      (ignore-errors (forward-char)))))
 
 (defun mk/forward-char (&optional count)
   "Moves forward COUNT or 1 chars but stays on current line."
   (interactive "p")
   (setq count (or count 1))
-  (deactivate-mark)
+  (mk--forward-char count))
+
+(defun mk/forward-char-keep (&optional count)
+  "Moves forward COUNT or 1 chars but stays on current line."
+  (interactive "p")
+  (setq count (or count 1))
+  (mk--forward-char count t))
+
+(defun mk--backward-char (count &optional keep)
+  "Moves backward COUNT or 1 chars but stays on current line."
   (dotimes (_ count)
+    (if keep
+        (unless mark-active
+          (set-mark (point)))
+      (deactivate-mark))
     (save-restriction
-      (narrow-to-region (point) (line-end-position))
-      (ignore-errors (forward-char count)))))
+      (narrow-to-region (point) (line-beginning-position))
+      (ignore-errors (backward-char)))))
 
 (defun mk/backward-char (&optional count)
   "Moves forward COUNT or 1 chars but stays on current line."
   (interactive "p")
   (setq count (or count 1))
-  (deactivate-mark)
-  (dotimes (_ count)
-    (save-restriction
-      (narrow-to-region (point) (line-beginning-position))
-      (ignore-errors (backward-char count)))))
+  (mk--backward-char count))
+
+(defun mk/backward-char-keep (&optional count)
+  "Moves forward COUNT or 1 chars but stays on current line."
+  (interactive "p")
+  (setq count (or count 1))
+  (mk--backward-char count t))
 
 (defun mk/next-line (&optional count)
   "Selects from cursor to next line."
@@ -87,7 +155,15 @@
 (define-key mckak-mode-map "v" 'mk/select-to-previous-word-end)
 (define-key mckak-mode-map "N" 'mc/mark-next-like-this)
 (define-key mckak-mode-map (kbd "M-s") 'mc/edit-lines)
-(define-key mckak-mode-map "s" 'mc/mark-all-in-region-regexp)
+(define-key mckak-mode-map "s" 'mc/mark-all-in-region-regexp-keep)
+(define-key mckak-mode-map "W" 'mk/select-to-next-word-start-keep)
+(define-key mckak-mode-map "E" 'mk/select-to-next-word-end-keep)
+;; (define-key mckak-mode-map "J" 'mk/next-line-keep)
+;; (define-key mckak-mode-map "K" 'mk/previous-line-keep)
+(define-key mckak-mode-map "H" 'mk/backward-char-keep)
+(define-key mckak-mode-map "L" 'mk/forward-char-keep)
+(define-key mckak-mode-map "B" 'mk/select-to-previous-word-start-keep)
+(define-key mckak-mode-map "V" 'mk/select-to-previous-word-end-keep)
 
 (dotimes (i 10)
   (define-key mckak-mode-map (number-to-string i) 'digit-argument))
